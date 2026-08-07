ac56a5c7590c1ab805a2a6c50333e585f8b6b362
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-08-07 20:29:55 +0200

    Add append condition if linked list empty

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,17 @@
-class LinkedList {}
+class LinkedList {
+  head;
+  tail;
+  append(value) {
+    if (tail !== undefined) {
+      const helpTail = tail;
+      tail = new Node(value, null);
+      helpTail.nextNode = tail;
+    } else {
+      tail = new Node(value, null);
+      head = tail;
+    }
+  }
+}
 
 class Node {
   constructor(value = null, nextNode = null) {
